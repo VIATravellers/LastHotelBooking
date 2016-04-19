65079c7084eea50cf0070a746ad9bddc5f270e48
OTHMAN007 <othmanajmi@outlook.dk>
2016-04-20 00:13:34 +0200

    Booking.java

---
+++ src/model/Booking.java
@@ -0,0 +1,72 @@
+package model;
+
+import java.util.*;
+
+import db.RoomDB;
+
+public class Booking
+{
+
+   private Guest guest;
+   private MyDate arrivalDate, departureDate;
+
+   private Room room;
+
+   public Booking(Guest name, MyDate arrivalDate2, MyDate departureDate2,
+         Room room)
+   {
+
+      this.guest = name;
+      this.arrivalDate = arrivalDate2;
+      this.departureDate = departureDate2;
+      this.room = room;
+
+   }
+
+   public Guest getGuest()
+   {
+      return guest;
+   }
+
+   public void setGuest(Guest guest)
+   {
+      this.guest = guest;
+   }
+
+   public MyDate getArrivalDate()
+   {
+      return arrivalDate;
+   }
+
+   public void setArrivalDate(MyDate arrivalDate)
+   {
+      this.arrivalDate = arrivalDate;
+   }
+
+   public MyDate getDepartureDate()
+   {
+      return departureDate;
+   }
+
+   public void setDepartureDate(MyDate departureDate)
+   {
+      this.departureDate = departureDate;
+   }
+
+   public Room getRoom()
+   {
+      return room;
+   }
+
+  
+
+   @Override
+   public String toString()
+   {
+
+      return "Booking  \n\tGuest name = " + guest.getName() + "\n\tArrival date = "
+            + arrivalDate + "\n\tDeparture date = " + departureDate + ",\n\tRoom: "
+            + room.toString() + "\n";
+   }
+
+}
